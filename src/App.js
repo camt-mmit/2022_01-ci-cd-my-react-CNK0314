cdf1f557586970ed96b74b1ec05670a4627a4431
Saranae <saranae@example.com>
2022-10-06 12:43:07 +0700

    deploy-react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ma Learn React Na
         </a>
       </header>
     </div>
